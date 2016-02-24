242aa7de543ffb4e076f49a762de2c6569cf755b
Jessica Fix and Jason Wuerch <wuerchjason@gmail.com>
2016-02-24 11:02:51 -0800

    Add twig to display list of cuisines

--- app/app.php
+++ app/app.php
@@ -4,12 +4,29 @@
 	require_once __DIR__.'/../src/Cuisine.php';
 	require_once __DIR__.'/../src/Restaurant.php';
 
+	$server = 'mysql:host=localhost;dbname=food';
+	$username = 'root';
+	$password = 'root';
+	$DB = new PDO($server, $username, $password);
+
 	$app = new Silex\Application();
 
 	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-	$app->get('/', function(){return 'Hello, World!';});
+	$app->get('/', function() use ($app) {
+		return $app['twig']->render('index.html.twig');
+	});
+
+	$app->post('/add_cuisine', function() use ($app) {
+		$name = $_POST['name'];
+		$test_cuisine = new Cuisine($name);
+		$test_cuisine->save();
+		$cuisines = Cuisine::getAll();
+		return $app['twig']->render('index.html.twig', array('cuisines' => $cuisines));
+	});
 
 	return $app;
+
+
 
 ?>
